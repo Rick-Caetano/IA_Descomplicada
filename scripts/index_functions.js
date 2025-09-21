db66fbdc0c13332cbda8e5de2e36ac66eaf9b12e
Rick-Caetano <rickbobcs9@gmail.com>
2025-09-21 20:49:35 -0300

    Aparecimento de barra no index agora funcional, alem de alteração no texto e estilo

--- scripts/index_functions.js
+++ scripts/index_functions.js
@@ -4,19 +4,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const secaoCapa = document.getElementById('capa');
     const containerDeRolagem = document.querySelector('.scroll-container');
 
-    function controlarVisibilidadeBarra() {
+    function controlaVisibilidadeBarra() {
         // Pega a posição da parte de baixo da seção "capa" em relação à janela.
-            const posicaoCapa = secaoCapa.getBoundingClientRect();
+        const posicaoCapa = secaoCapa.getBoundingClientRect();
 
         // Se a parte de baixo da capa já passou do topo da tela, rolamos para além dela
         if (posicaoCapa.bottom < 0) {
             barraTopo.classList.add('visivel');
+            barraTopo.classList.remove('escondido');
         } else {
             barraTopo.classList.remove('visivel');
+            barraTopo.classList.add('escondido');
         }
     }
 
-    // 3. "escutador" ao evento de rolagem DO SEU CONTAINER
-    containerDeRolagem.addEventListener('scroll', controlarVisibilidadeBarra);
+    containerDeRolagem.addEventListener('scroll', controlaVisibilidadeBarra);
 
 });
