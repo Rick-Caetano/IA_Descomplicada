0c8ecba7b998ab3638f59addafda018ea5d53a96
Rick-Caetano <rickbobcs9@gmail.com>
2025-09-21 19:49:52 -0300

    update index_functions.js

--- scripts/index_functions.js
+++ scripts/index_functions.js
@@ -1,17 +1,22 @@
-const barraTopo = document.getElementById('barra-topo');
-const secaoCapa = document.getElementById('capa');
+document.addEventListener('DOMContentLoaded', () => {
 
-function controlarVisibilidadeBarra() {
-    // Pega a posição da parte de baixo da seção "capa"
-    const capaBottom = secaoCapa.getBoundingClientRect().bottom;
+    const barraTopo = document.getElementById('barra-topo');
+    const secaoCapa = document.getElementById('capa');
+    const containerDeRolagem = document.querySelector('.scroll-container');
 
-    // Se a parte de baixo da capa já passou do topo da tela, rolamos para além dela
-    if (capaBottom < 0) {
-        barraTopo.classList.add('visivel');
-    } else {
-        barraTopo.classList.remove('visivel');
+    function controlarVisibilidadeBarra() {
+        // Pega a posição da parte de baixo da seção "capa" em relação à janela.
+            const posicaoCapa = secaoCapa.getBoundingClientRect();
+
+        // Se a parte de baixo da capa já passou do topo da tela, rolamos para além dela
+        if (posicaoCapa.bottom < 0) {
+            barraTopo.classList.add('visivel');
+        } else {
+            barraTopo.classList.remove('visivel');
+        }
     }
-}
 
-// 3. "escutador" ao evento de rolagem DO SEU CONTAINER
-container.addEventListener('scroll', controlarVisibilidadeBarra);
+    // 3. "escutador" ao evento de rolagem DO SEU CONTAINER
+    containerDeRolagem.addEventListener('scroll', controlarVisibilidadeBarra);
+
+});
